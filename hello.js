797976e041794ef3f57e98287a6466179d7a2baf
Jules Bloux <jules.bloux@innovia-france.com>
2023-10-30 14:19:13 +0100

    time performance comparison between classic for, map and find

--- hello.js
+++ hello.js
@@ -1,1 +1,35 @@
-console.log("hello");
+
+
+
+
+const Array = [];
+const goal = 10000000;
+
+for(let i = 0; i < goal; i++){
+    Array.push(i);
+}
+
+console.time("find")
+console.log("find");
+Array.find((el) => el === goal)
+console.timeEnd("find");
+
+
+console.time('map');
+console.log("map")
+Array.map((el)=>{
+    if(el === goal){
+        return el;
+    }
+})
+console.timeEnd("map");
+
+
+console.time('classic for')
+console.log("classic for")
+for(let i = 0; i < Array.length; i++){
+    if(Array[i] === goal) {
+        break
+    }
+}
+console.timeEnd("classic for");
